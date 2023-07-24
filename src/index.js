50234bfe65b6aa473e1a21fec9b9773832e99082
Ali Nayebi <elderekkoo@gmail.com>
2023-07-24 12:38:04 +0330

    font update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./assets/fonts/Webfonts/fontiran.css";
+import "./assets/fonts/fontiran.css";
 import { getDirection } from "./localization/index";
 document.getElementsByTagName('body')[0].setAttribute('dir', getDirection());
 
